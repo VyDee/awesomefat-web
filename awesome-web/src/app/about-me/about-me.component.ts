e88e6a77517abe545c132b9b46ac75435bfd63e4
VyDee <vy.duong@cuw.edu>
2020-09-12 11:42:03 -0500

    fix about me bug

--- awesome-web/src/app/about-me/about-me.component.ts
+++ awesome-web/src/app/about-me/about-me.component.ts
@@ -1,5 +1,5 @@
 import { Routes } from '@angular/router';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener, ElementRef } from '@angular/core';
 import { ViewportScroller } from '@angular/common';
 
 @Component({
@@ -9,22 +9,49 @@ import { ViewportScroller } from '@angular/common';
 })
 export class AboutMeComponent implements OnInit {
   currentSection = 'about-me';
+  spiedTag = 'DIV';
 
   constructor(
     private viewportScroller: ViewportScroller,
+    private _el: ElementRef,
   ) { }
 
   ngOnInit(): void {
   }
 
-  public onSectionChange(sectionId: string) {
-    this.currentSection = sectionId;
-    console.log("onSectionChange ",sectionId);
-  }
+  // public onSectionChange(sectionId: string) {
+  //   this.currentSection = sectionId;
+  //   console.log("onSectionChange ",sectionId);
+  // }
 
   public onClick(elementId: string) {
     this.currentSection = elementId;
     this.viewportScroller.scrollToAnchor(elementId);
-    console.log(this.viewportScroller.scrollToAnchor(elementId))
+    console.log(this.viewportScroller.scrollToAnchor(elementId));
+  }
+
+  @HostListener('window: scroll', ['$event'])
+  onScroll() {
+    //current scroll posisiton on the Y axis
+    const verticalOffset = window.pageYOffset + 100 || 0;
+
+    //get aws-content children
+    const children = this._el.nativeElement.children[0].children[1].children;
+    console.log(">> _el.nativeElement.child[0]: ", this._el.nativeElement.children[0]);
+    console.log(">>> ..... children[1] ", this._el.nativeElement.children[0].children[1]);
+    console.log(">>> ..... children[1].children ", children);
+
+    for (let i = 0; i < children.length; i ++) {
+      const element = children [i];
+      console.log(element);
+
+      console.log(element.tagName);
+      if(element.tagName === this.spiedTag) {
+        // compare element position counting from the top with the vertical offset
+        if (element.offsetTop < verticalOffset) {
+          this.currentSection = element.id;
+        }
+      }
+    }
   }
 }
