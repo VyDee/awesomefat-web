ee33413159423521bf70fbe01bdf5a53f2af0c2b
VyDee <vy.duong@cuw.edu>
2020-10-16 15:51:09 -0500

    adding a cart to service page

--- awesome-web/src/app/aws-service/coaching-service/coaching-service.component.ts
+++ awesome-web/src/app/aws-service/coaching-service/coaching-service.component.ts
@@ -1,5 +1,8 @@
-import { CoachingService } from './../../service/coaching.service';
-import { Component, OnInit } from '@angular/core';
+import { CartComponent } from './cart/cart.component';
+import { CoachingService } from '../../service/coaching.service';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { ShoppingService } from 'src/app/service/shopping.service';
+import { UserAuthService } from 'src/app/service/user-auth.service';
 
 @Component({
   selector: 'app-coaching-service',
@@ -7,17 +10,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./coaching-service.component.scss']
 })
 export class CoachingServiceComponent implements OnInit {
+  @ViewChild(CartComponent) cart: CartComponent;
 
   coachingServiceArr = [];
+  totalNumber: number;
 
   constructor(
-    private coachingService: CoachingService
+    private coachingService: CoachingService,
+    private shoppingService: ShoppingService,
+    public userAuthService: UserAuthService,
   ) {}
 
   ngOnInit(): void {
     this.coachingService.getCoachingServices().subscribe((services) => {
       this.coachingServiceArr = services;
-    })
+    });
+    const userUID = localStorage.getItem('userUID');
+    if (this.userAuthService.isAuthenticated())
+    {
+      this.shoppingService.getOrders().subscribe((orders) => {
+        const totalOrders = orders.filter(x => x.userUID === userUID && x.isPaid === false);
+        this.totalNumber = totalOrders.length;
+      });
+    }
   }
 
 
