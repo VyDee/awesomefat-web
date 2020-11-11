d17389c54cddc5cf403bf0a1d08c3c3954f0bb58
VyDee <vy.duong@cuw.edu>
2020-11-10 18:09:29 -0600

    add env

--- nodemailer/sendgrid.js
+++ nodemailer/sendgrid.js
@@ -1,73 +1,72 @@
-const Sendgrid = require('sendgrid')(
-    process.env.SENDGRID_API_KEY || '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'
-  );
+const Sendgrid = require('sendgrid')(process.env.SENDGRID_API_KEY);
+require('dotenv').config()
 
-  const express = require('express');
-  const bodyParser = require('body-parser');
-  const app = express();
-  var cors = require('cors');
-  
-  // parse application/x-www-form-urlencoded
-  app.use(bodyParser.urlencoded({ extended: true }));
-  app.use(bodyParser.json());
-  app.use(cors());
-  
-  app.post('/send-mail', function (req, res) {
-    // PUT your send mail logic here, req.body should have your fsubmitted form's values
-    let request = Sendgrid.emptyRequest({
-      "method": "POST",
-      "path": "/v3/mail/send",
-      body: {
-        template_id:"d-2b31c3de7ee44d1ca3027c5b7d8f5cf7",
-        personalizations: [
-            {
-                to: [{ email: 'vy.duong@cuw.edu' }],
-                subject: 'Sendgrid test email from Node.js',
-                dynamic_template_data: {
-                  name:"Vy"
-                },
-            }
-        ],
-        from: { email: 'vy.duong@cuw.edu' },
-      }
-    });
-    sendMail(request);
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
-    res.send('SEND MAIL');  
-  })
-  
-  app.listen(3000, function () {
-    console.log('LISTENING on port 3000');
-  })
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+var cors = require('cors');
 
-  function sendMail(request) { 
-    // console.log(formData);
-    // let request = Sendgrid.emptyRequest({
-    //   "method": "POST",
-    //   "path": "/v3/mail/send",
-    //   body: {
-    //     template_id:"d-2b31c3de7ee44d1ca3027c5b7d8f5cf7",
-    //     personalizations: [
-    //         {
-    //             to: [{ email: 'vy.duong@cuw.edu' }],
-    //             subject: 'Sendgrid test email from Node.js',
-    //             dynamic_template_data: {
-    //               name:"Vy"
-    //             },
-    //         }
-    //     ],
-    //     from: { email: 'vy.duong@cuw.edu' },
-    //   }
-    // });
-  
-    Sendgrid.API(request, function (error, response) {
-      console.log(JSON.stringify(response));
-      if (error) {
-        console.log('Mail not sent; see error message below.');
-      } else {
-        console.log('Mail sent successfully!');
-      }
-      console.log(response);
-    });
-  }
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cors());
+
+app.post('/send-mail', function (req, res) {
+  // PUT your send mail logic here, req.body should have your fsubmitted form's values
+  let request = Sendgrid.emptyRequest({
+    "method": "POST",
+    "path": "/v3/mail/send",
+    body: {
+      template_id:"d-2b31c3de7ee44d1ca3027c5b7d8f5cf7",
+      personalizations: [
+          {
+              to: [{ email: 'vy.duong@cuw.edu' }],
+              subject: 'Sendgrid test email from Node.js',
+              dynamic_template_data: {
+                name:"Vy"
+              },
+          }
+      ],
+      from: { email: 'vy.duong@cuw.edu' },
+    }
+  });
+  sendMail(request);
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.send('SEND MAIL');  
+})
+
+app.listen(3000, function () {
+  console.log('LISTENING on port 3000');
+})
+
+function sendMail(request) { 
+  // console.log(formData);
+  // let request = Sendgrid.emptyRequest({
+  //   "method": "POST",
+  //   "path": "/v3/mail/send",
+  //   body: {
+  //     template_id:"d-2b31c3de7ee44d1ca3027c5b7d8f5cf7",
+  //     personalizations: [
+  //         {
+  //             to: [{ email: 'vy.duong@cuw.edu' }],
+  //             subject: 'Sendgrid test email from Node.js',
+  //             dynamic_template_data: {
+  //               name:"Vy"
+  //             },
+  //         }
+  //     ],
+  //     from: { email: 'vy.duong@cuw.edu' },
+  //   }
+  // });
+
+  Sendgrid.API(request, function (error, response) {
+    console.log(JSON.stringify(response));
+    if (error) {
+      console.log('Mail not sent; see error message below.');
+    } else {
+      console.log('Mail sent successfully!');
+    }
+    console.log(response);
+  });
+}
